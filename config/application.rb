d65cab14ce769578d11e7624709207036b0c3a51
Noel del Castillo <ngdelcastillo@gmail.com>
2012-08-25 06:11:11 +0800

    set precompile assets to false

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,7 @@ module Volunteer
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.logger = false
+
+    config.assets.initialize_on_precompile = false
   end
 end
